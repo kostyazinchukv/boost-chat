2440a5bfa56c1faadeaad8b794bcbb344388368f
kostyazinchukv <heihachithegod@gmail.com>
2021-05-27 17:17:02 +0300

    fixup! Build switched to cmake

---
+++ build/CMakeFiles/CHAT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CHAT"
+  "CHAT.pdb"
+  "CMakeFiles/CHAT.dir/src/client.cpp.o"
+  "CMakeFiles/CHAT.dir/src/mainClient.cpp.o"
+  "CMakeFiles/CHAT.dir/src/mainServer.cpp.o"
+  "CMakeFiles/CHAT.dir/src/server.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CHAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
