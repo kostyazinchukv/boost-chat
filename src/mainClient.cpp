5482bb9ac3ef3bb39d4c323eca5deac091bbca4a
kostyazinchukv <heihachithegod@gmail.com>
2021-06-04 17:47:18 +0300

    fixup! clang-tidy. client refactored

--- src/mainClient.cpp
+++ src/mainClient.cpp
@@ -1,7 +1,8 @@
 #include "client.hpp"
-
+#define PORT 9999
 int main() {
-  Client client(9999, "127.0.0.1", "Hello, from client!");
+  int port = PORT;
+  Client client(port, "127.0.0.1" /* NOLINT*/, "Hello, from client!"/* NOLINT*/);
   client.menu();
   return 0;
 }
